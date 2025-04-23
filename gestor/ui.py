1e12c41388cf5d51d7e62e94df61ed1d5c1498ac
zair <oliveracisneros1@gmail.com>
2025-04-23 07:26:39 +0000

    cambios

--- gestor/ui.py
+++ gestor/ui.py
@@ -1,11 +1,21 @@
 from tkinter import *
 from .helpers import limpiar_pantalla
 
-class MainWindow(Tk):
+class CenterMixin:
+    def centrar(self):
+        self.update_idletasks()
+        ancho = self.winfo_width()
+        alto = self.winfo_height()
+        x = (self.winfo_screenwidth() // 2) - (ancho // 2)
+        y = (self.winfo_screenheight() // 2) - (alto // 2)
+        self.geometry(f"{ancho}x{alto}+{x}+{y}")
+
+class MainWindow(Tk, CenterMixin):
     def __init__(self):
         super().__init__()
         self.title('Gestor de Clientes')
         self.geometry("600x400")
+        self.centrar()
         self.build()
 
     def build(self):
