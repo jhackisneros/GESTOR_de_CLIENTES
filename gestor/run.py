c231be91ff0e286bde28dc5ad148b7f522c40bb4
zair <oliveracisneros1@gmail.com>
2025-04-23 07:14:44 +0000

    cambios

--- gestor/run.py
+++ gestor/run.py
@@ -1,4 +1,10 @@
+import sys
 from . import menu
+from . import ui
 
 def main():
-    menu.iniciar()
+    if len(sys.argv) > 1 and sys.argv[1] == "-t":
+        menu.iniciar()
+    else:
+        app = ui.MainWindow()
+        app.mainloop()
